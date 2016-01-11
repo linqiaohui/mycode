19232a1c8f42dacc8e60b2d8a41b04fbd3a04f2a
ym <linqiaohui1995@126.com>
2016-01-11 17:07:30 +0800

    pgsql

---
+++ SQL/pgsql/plsql存储过程创建方法.sql
@@ -0,0 +1,17 @@
+﻿CREATE OR REPLACE FUNCTION born_userid()
+RETURN int AS
+$BODY$
+    DECLARE
+        scratch_number int;
+        user_id int;
+    BEGIN
+        scratch_number = SELECT count(*) FROM userid_scratch;
+        IF (scratch_number > 0) 
+        THEN
+            user_id = SELECT min(id_scratch) FROM userid_scratch;
+            RETURN user_id;
+        ELSE
+            user_id = SELECT MAX(user_id)+1 FROM user_login;
+            RETURN user_id;
+    END
+$BODY$
---
+++ SQL/pgsql/plsql设置自增.sql
@@ -0,0 +1,9 @@
+﻿--设置自赠字段
+create sequence user_id
+start with 6
+increment by 1
+no minvalue
+no maxvalue
+cache 1;
+
+alter table user_login alter column user_id set default nextval('user_id');
